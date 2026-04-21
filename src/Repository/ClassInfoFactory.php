b7c0a4d563a618d1007531202f2a1f885a5de6ae
Eric Stern <eric@ericstern.com>
2026-04-21 11:18:32 -0700

    Add ClassInfoFactory interface

---
+++ src/Repository/ClassInfoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Firehed\PhpLsp\Repository;
+
+use Firehed\PhpLsp\Domain\ClassInfo;
+use PhpParser\Node\Stmt;
+use ReflectionClass;
+
+/**
+ * Creates ClassInfo domain objects from AST nodes or Reflection.
+ */
+interface ClassInfoFactory
+{
+    /**
+     * @param Stmt\ClassLike $node The AST node for the class/interface/trait/enum
+     * @param string $uri The file URI where this class is defined
+     */
+    public function fromAstNode(Stmt\ClassLike $node, string $uri): ClassInfo;
+
+    /**
+     * @param ReflectionClass<object> $class
+     */
+    public function fromReflection(ReflectionClass $class): ClassInfo;
+}
