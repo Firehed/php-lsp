6cc23335909fd9f1972c8b81069312e5b4d1b696
Eric Stern <eric@ericstern.com>
2026-04-28 14:36:03 -0700

    Add UnionType implementation

---
+++ src/Domain/UnionType.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Firehed\PhpLsp\Domain;
+
+final readonly class UnionType implements Type
+{
+    /**
+     * @param list<Type> $members
+     */
+    public function __construct(
+        private array $members,
+    ) {
+    }
+
+    public function format(): string
+    {
+        $parts = array_map(function (Type $member): string {
+            $formatted = $member->format();
+            if ($member instanceof IntersectionType) {
+                return '(' . $formatted . ')';
+            }
+            return $formatted;
+        }, $this->members);
+        return implode('|', $parts);
+    }
+
+    public function getResolvableClassNames(): array
+    {
+        $classNames = [];
+        foreach ($this->members as $member) {
+            $classNames = array_merge($classNames, $member->getResolvableClassNames());
+        }
+        return $classNames;
+    }
+
+    public function isNullable(): bool
+    {
+        foreach ($this->members as $member) {
+            if ($member->isNullable()) {
+                return true;
+            }
+            if ($member instanceof NamedType && $member->format() === 'null') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
