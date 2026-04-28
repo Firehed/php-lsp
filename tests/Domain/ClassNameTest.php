f97a631d422342b97dcc89baeaf7de56cf4b6076
Eric Stern <eric@ericstern.com>
2026-04-28 15:49:02 -0700

    Extend ClassName to implement Type

--- tests/Domain/ClassNameTest.php
+++ tests/Domain/ClassNameTest.php
@@ -57,4 +57,24 @@ class ClassNameTest extends TestCase
         $b = new ClassName($lowercased);
         self::assertTrue($a->equals($b));
     }
+
+    public function testFormatReturnsFqn(): void
+    {
+        $cn = new ClassName(ClassName::class);
+        self::assertSame(ClassName::class, $cn->format());
+    }
+
+    public function testGetResolvableClassNamesReturnsItself(): void
+    {
+        $cn = new ClassName(\stdClass::class);
+        $classNames = $cn->getResolvableClassNames();
+        self::assertCount(1, $classNames);
+        self::assertSame($cn, $classNames[0]);
+    }
+
+    public function testIsNullableReturnsFalse(): void
+    {
+        $cn = new ClassName(\stdClass::class);
+        self::assertFalse($cn->isNullable());
+    }
 }
