6a9e2e45f1cd243dc80815a5a864f44b503a8897
Eric Stern <eric@ericstern.com>
2026-04-28 14:36:03 -0700

    Add IntersectionType implementation

---
+++ src/Domain/IntersectionType.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Firehed\PhpLsp\Domain;
+
+final readonly class IntersectionType implements Type
+{
+    /**
+     * @param list<Type> $members
+     */
+    public function __construct(
+        private array $members,
+    ) {
+    }
+
+    public function format(): string
+    {
+        $parts = array_map(
+            fn (Type $member): string => $member->format(),
+            $this->members,
+        );
+        return implode('&', $parts);
+    }
+
+    public function getResolvableClassNames(): array
+    {
+        $classNames = [];
+        foreach ($this->members as $member) {
+            $classNames = array_merge($classNames, $member->getResolvableClassNames());
+        }
+        return $classNames;
+    }
+
+    public function isNullable(): bool
+    {
+        return false;
+    }
+}
