5454af85a009fbd0198782a15e7e462c8667e4eb
Eric Stern <eric@ericstern.com>
2026-07-08 15:39:10 -0700

    Add CompletionItemKind enum for LSP kind values

---
+++ src/Completion/CompletionItemKind.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Firehed\PhpLsp\Completion;
+
+/**
+ * LSP CompletionItemKind values.
+ *
+ * @see https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#completionItemKind
+ */
+enum CompletionItemKind: int
+{
+    case Method = 2;
+    case Function = 3;
+    case Field = 5;
+    case Variable = 6;
+    case Class_ = 7;
+    case Property = 10;
+    case Keyword = 14;
+    case EnumMember = 20;
+    case Constant = 21;
+}
