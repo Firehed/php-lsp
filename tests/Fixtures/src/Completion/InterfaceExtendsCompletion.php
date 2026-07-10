b6ff446e90e6bfc5f77b53f7eb203de448ad9f14
Eric Stern <eric@ericstern.com>
2026-07-09 21:22:58 -0700

    Add integration tests for interface extends completion (#312)

---
+++ tests/Fixtures/src/Completion/InterfaceExtendsCompletion.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fixtures\Completion;
+
+use Fixtures\Domain\Entity;
+use Fixtures\Domain\User;
+use Fixtures\Traits\SingletonTrait;
+
+interface InterfaceExtendsCompletion extends /*|interface_extends_empty*/
+{
+}
---
+++ tests/Fixtures/src/Completion/InterfaceExtendsListCompletion.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fixtures\Completion;
+
+use Fixtures\Domain\Describable;
+use Fixtures\Domain\Entity;
+
+interface InterfaceExtendsListCompletion extends Entity, D/*|interface_extends_list*/
+{
+}
