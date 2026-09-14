3baa480bc0e2ffde9f0885877a38d3feee0ade4f
Eric Stern <eric@ericstern.com>
2026-09-14 14:38:27 -0700

    Expand server definition to prepare inlining

--- config/server.php
+++ config/server.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Firehed\PhpLsp;
 
+use Firehed\Container\TypedContainerInterface as TC;
+
 return [
     Protocol\ServerInfo::class => fn () => new Protocol\ServerInfo('php-lsp', '0.1.0'),
 
-    Server::class => fn ($c) => Server::forProject(
-        $c->get(Transport\TransportInterface::class),
-        $c->get(Protocol\ServerInfo::class),
-    ),
+    Server::class => function (TC $c) {
+        return Server::forProject(
+            $c->get(Transport\TransportInterface::class),
+            $c->get(Protocol\ServerInfo::class),
+        );
+    }
 ];
